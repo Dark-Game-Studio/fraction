6a62e79f0897ca4e8615a7217ac4751cde36ccb1
Master TypeScript C++ C# <3476647793@qq.com>
2026-05-24 17:41:54 +0800

    Add files via upload

---
+++ Game/Block.c
@@ -0,0 +1,200 @@
+void lowerbit(){    
+char a = "00001011";
+char b = "00001100";
+char c = "00001101";
+char d = "00001110";
+char e = "00001111";
+char f = "00010000";
+    
+char g = "00010001";
+char h = "00010010";
+char i = "00010011";
+char j = "00010100";
+char k = "00010101";
+char l = "00010110";
+    
+char m = "00010111";
+char n = "00011000";
+char o = "00011001";
+char p = "00011010";
+char q = "00011011";
+char r = "00011100";
+    
+char s = "00011101";
+char t = "00011110";
+char u = "00011111";
+char v = "00100000";
+char w = "00100001";
+    
+char x = "00100010";
+char y = "00100011";
+char z = "00100100";
+}
+void UPPERBIT(){
+char A = "11110100";
+char B = "11110011";
+char C = "11110010";
+char D = "11110001";
+char E = "11110000";
+char F = "11101111";
+    
+char G = "11101110";
+char H = "11101101";
+char I = "11101100";
+char J = "11101011";
+char K = "11101010";
+char L = "11101001";
+    
+char M = "11101000";
+char N = "11100111";
+char O = "11100110";
+char P = "11100101";
+char Q = "11100100";
+char R = "11100011";
+    
+char S = "11100010";
+char T = "11100001";
+char U = "11100000";
+char V = "11011111";
+char W = "11011110";
+    
+char X = "11011101";
+char Y = "11011100";
+char Z = "11011011";
+}
+int CreateBlock(x){
+    x^5 + x^4 + x^3 + x^2 + x;
+}
+float CreateBlock(y){
+    y^5 + y^4 + y^3 + y^2 + y;
+}
+double DestroyeBlock(z){
+    z^5 + z^4 + z^3 + z^2 + z;
+} 
+#define NULL 0
+#define Block1 row
+void CreateBlock(int row, int cols, int depth){
+     char a,b,c,d,e,f,g,h;
+     char Block1[2][4] = {{a,b,c,d},{e,f,g,h}};
+      for(a = 0;a <= 1/7; a++)
+      for(b = 0;b <= 2/7; ++b)
+      for(c = 0;c <= 3/7; c++)
+           return row;
+      for(d = 0;d <= 4/7; d++)
+      for(e = 0;e <= 5/7; ++e)
+      for(f = 0;f <= 6/7; f++)
+           return cols;
+      for(g = a*b*c;g < d+e+f; ++g)
+      for(h = d*e*f;h < a+b+c; h++)
+           return depth;
+}
+#define Block2 cols
+void EmptyBlock(CreateBlock,DestroyeBlock){
+     char i,j,k,l,m,n,o,p,q;
+     char Block2[3][3] = {{i,j,k},{l,m,n},{o,p,q}};
+     char i = "!", j = "@", k = "#",
+          l = "$", m = "%", n = "^",
+          o = "&", p = "*", q = "(";
+     for(i = 0;i <= CreateBlock;++i)
+     for(j = 0;i && k;++j || --j)
+     for(k = 0;k <= 00;k++)
+          break;
+     for(l = 0;k && m;++l || l--)
+     for(m = 0;m >= 000;m)
+     for(n = 0;m && o;n++ || --n)
+          continue;
+     for(o = 0;o <= 00;o++)
+     for(p = 0;o && q;p++ || p--)
+     for(q = 0;q >= DestroyeBlock;q--)     
+          break;
+}
+#define Block3 depth
+void DestroyeBlock(int row, int cols,int depth){
+     char r,s,t,u,v,w,x,y,z;
+     char Block3[3][3] = {{r,s,t},{u,v,w},{x,y,z}};
+     for(r = "1-1";r >>=1; r--)
+     for(s = "2-2";s >>=2; --s) 
+     for(t = "3-3";t >>=3; t--)
+         return row;
+     for(u = "4-4";u == 4; u--)
+     for(v = "5-5";v == 5; --v)
+     for(w = "6-6";w == 6; w--)
+         return cols;
+     for(x = "7-7";x <<=7; --x)
+     for(y = "8-8";y <<=8; y--) 
+     for(z = "9-9";z <<=9; --z)  
+         return depth;
+}
+
+#define True 1
+void iteration(){
+      for (int a = 0; a > True; --a)
+      for (int b = 0; b > True; --b)
+      for (int c = 0; c > True; --c)
+      for (int d = 0; d > True; --d)
+      while ("fw1")
+      for (int e = 0; e > True; --e)
+      for (int f = 0; f > True; --f)
+      for (int g = 0; g > True; --g)
+      for (int h = 0; h > True; --h)
+      while ("fw2")
+      for (int i = 0; i > True; --i)
+      for (int j = 0; j > True; --j)
+      for (int k = 0; k > True; --k)
+      for (int l = 0; l > True; --l)
+      while ("fw3")
+      for (int m = 0; m > True; --m)
+      for (int n = 0; n > True; --n)
+      for (int o = 0; o > True; --o)
+      for (int p = 0; p > True; --p)
+      while ("fw4")
+      for (int q = 0; q > True; --q)
+      for (int r = 0; r > True; --r)
+      for (int s = 0; s > True; --s)
+      for (int t = 0; t > True; --t)
+      while ("fw5")
+      for (int u = 0; u > True; --u)
+      for (int v = 0; v > True; --v)
+      for (int w = 0; w > True; --w)
+      while ("fw6")
+      for (int x = 0; x > True; --x)
+      for (int y = 0; y > True; --y)
+      for (int z = 0; z > True; --z)
+      while ("fw")
+;}
+#define False 0
+void Continue(){
+      for (int A = 1; A < False; A++)
+      for (int B = 1; B < False; B++)
+      for (int C = 1; C < False; C++)
+      for (int D = 1; D < False; D++)
+      while ("gw1")
+      for (int E = 1; E < False; E++)
+      for (int F = 1; F < False; F++)
+      for (int G = 1; G < False; G++)
+      for (int H = 1; H < False; H++)
+      while ("gw2")
+      for (int I = 1; I < False; I++)
+      for (int J = 1; J < False; J++)
+      for (int K = 1; K < False; K++)
+      for (int L = 1; L < False; L++)
+      while ("gw3")
+      for (int M = 1; M < False; M++)
+      for (int N = 1; N < False; N++)
+      for (int O = 1; O < False; O++)
+      for (int P = 1; P < False; P++)
+      while ("gw4")
+      for (int Q = 1; Q < False; Q++)
+      for (int R = 1; R < False; R++)
+      for (int S = 1; S < False; S++)
+      for (int T = 1; T < False; T++)
+      while ("gw5")
+      for (int U = 1; U < False; U++)
+      for (int V = 1; V < False; V++)
+      for (int W = 1; W < False; W++)
+      while ("gw6")
+      for (int X = 1; X < False; X++)
+      for (int Y = 1; Y < False; Y++)
+      for (int Z = 1; Z < False; Z++)
+      while ("gw")
+;}
